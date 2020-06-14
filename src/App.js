dd0680c2758adb15d856ee9cc0dbb76d7dbc2d58
Alba <alba.bella@cix.csi.cuny.edu>
2020-06-14 12:32:00 -0400

    Decrement

--- src/App.js
+++ src/App.js
@@ -9,24 +9,7 @@ class App extends Component {
   render() {
     return (
     <>
-      <ContactCard
-       name= "John" 
-       mobile="212555555" 
-       work="1800222222" 
-       email="hello@example.com"
-      /> 
-      <ContactCard
-       name= "Ann" 
-       mobile="212555555" 
-       work="1800222222" 
-       email="ann@example.com"
-      />
-      <ContactCard
-       name= "Serena" 
-       mobile="212555555" 
-       work="1800222222" 
-       email="serena@example.com"
-      />
+      <Decrement start={5}/>
     </>
   );
   }
